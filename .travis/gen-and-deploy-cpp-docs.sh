6fbb350e811cd2aa7224a6e5682b02cf57306fac
Brian Heim <brianlheim@gmail.com>
2017-06-27 00:15:36 -0400

    Change doxyfile location, add debuggin code

--- .travis/gen-and-deploy-cpp-docs.sh
+++ .travis/gen-and-deploy-cpp-docs.sh
@@ -65,11 +65,14 @@ rm -rf *
 # to NO, which it is by default. So creating the file just in case.
 echo "" > .nojekyll
 
+echo "ls -a:"
+ls -a
+
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
 # Redirect both stderr and stdout to the log file AND the console.
-doxygen ../DOXYFILE 2>&1 | tee doxygen.log
+doxygen DOXYFILE 2>&1 | tee doxygen.log
 
 ################################################################################
 ##### Upload the documentation to the gh-pages branch of the repository.   #####
