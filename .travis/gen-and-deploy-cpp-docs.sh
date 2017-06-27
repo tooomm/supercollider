8fccb9a7b918382cf4ab471a51500358eac62b8d
Brian Heim <brianlheim@gmail.com>
2017-06-27 09:46:14 -0400

    Travis: Echo credentials to figure out why auth fails

--- .travis/gen-and-deploy-cpp-docs.sh
+++ .travis/gen-and-deploy-cpp-docs.sh
@@ -90,6 +90,7 @@ if [ -d "html" ] && [ -f "html/index.html" ]; then
   # Force push to the remote gh-pages branch.
   # The ouput is redirected to /dev/null to hide any sensitive credential data
   # that might otherwise be exposed.
+  echo "https://${GH_REPO_TOKEN}@${GH_REPO_REF}"
   git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" 2>&1
 else
   echo '' >&2
