e28aa75e7a6b563bfb21f6582d9a3beb911d3550
Brian Heim <brianlheim@gmail.com>
2017-08-18 11:50:58 -0400

    travis: add comments to scripts & config

--- .travis/qpm-prep.sh
+++ .travis/qpm-prep.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# checkout qpm from scztt's repo
 cd $TRAVIS_BUILD_DIR/BUILD
 sudo pip install git+https://github.com/scztt/qpm.git@qpm-unit
+
+# install CommonTests, CommonTestsGUI
 qpm quark checkout CommonTests CommonTestsGUI --location $HOME/Quarks
+
+# move the config file into the build directory
 cp ../travis_test_run_proto.json ./travis_test_run.json
--- .travis/test.sh
+++ .travis/test.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# get the location of the sclang executable
 if [[ $TRAVIS_OS_NAME == linux ]]; then
 	export SCLANG=$TRAVIS_BUILD_DIR/BUILD/Install/bin/sclang
 else
 	export SCLANG=$TRAVIS_BUILD_DIR/BUILD/Install/SuperCollider/SuperCollider.app/Contents/MacOS/sclang
 fi
 
+# steps needed to make non-Qt builds work
 if [[ $QT != true ]]; then $TRAVIS_BUILD_DIR/.travis/fix-non-Qt.sh; fi
 
+# test that sclang at least boots and interprets
 $TRAVIS_BUILD_DIR/testsuite/sclang/launch_test.py $SCLANG
 
+# launch the test suite through qpm
 if [[ $QT == true ]]; then $TRAVIS_BUILD_DIR/.travis/qpm-test.sh; fi
