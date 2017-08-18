778ea2664632418e6cd97ba9b331390f57eb30f6
Brian Heim <brianlheim@gmail.com>
2017-08-17 21:13:16 -0400

    travis: add comments to config & scripts

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 brew update
-brew tap homebrew/versions
+brew tap homebrew/versions # this can be removed - qt55 was migrated to core
 brew outdated cmake || brew upgrade cmake
-brew install libsndfile python || true
+brew install libsndfile python || true # python is already installed. this can also be specified in main matrix
 brew install qt55 || true
 brew link qt55 --force
