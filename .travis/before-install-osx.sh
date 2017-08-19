c761e6cd12d6d13541ccaec256d7e1f8c175c3d0
Brian Heim <brianlheim@gmail.com>
2017-08-19 12:37:14 -0400

    travis: attempt to add ccache caching

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -2,6 +2,11 @@
 
 brew update
 brew outdated cmake || brew upgrade cmake
+
+# according to https://docs.travis-ci.com/user/caching#ccache-cache
+brew install ccache
+export PATH="/usr/local/opt/ccache/libexec:$PATH"
+
 brew install libsndfile || true
 brew install qt55 || true
 brew link qt55 --force
