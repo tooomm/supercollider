e529d88e282f56c968a75b447c03605013997340
Brian Heim <brianlheim@gmail.com>
2017-08-19 11:46:11 -0400

    travis: remove unnecessary steps from osx install script

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 brew update
-brew tap homebrew/versions # this can be removed - qt55 was migrated to core
 brew outdated cmake || brew upgrade cmake
-brew install libsndfile python || true # python is already installed. this can also be specified in main matrix
+brew install libsndfile || true
 brew install qt55 || true
 brew link qt55 --force
