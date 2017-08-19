d46b06a45d23f6498719d9a02352869f43d8d3c5
Brian Heim <brianlheim@gmail.com>
2017-08-19 14:28:52 -0400

    travis: attempt to get by without brew update

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-brew update
 brew outdated cmake || brew upgrade cmake
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
-brew install ccache
+brew install ccache libsndfile readline
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
 
-brew install libsndfile || true
 brew install qt55 || true
 brew link qt55 --force
