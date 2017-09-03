810c5c88cdc1b788a0c050a4fcc217f577a115f0
Brian Heim <brianlheim@gmail.com>
2017-09-03 00:04:13 -0400

    travis tests: attempt headless linux fix

--- .travis/run-sc-class-library-test-suite.sh
+++ .travis/run-sc-class-library-test-suite.sh
@@ -4,6 +4,12 @@
 # Tests in testsuite/classlibrary/server are ignored,
 # as are tests for the UnitTesting Quark itself.
 
+# accommodate headless setup on Linux
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then
+	export QT_PLATFORM_PLUGIN=offscreen
+fi
+
 if "$SCLANG" "$TRAVIS_BUILD_DIR/.travis/configureTestEnvironment.scd"
 then
 	"$SCLANG" "$TRAVIS_BUILD_DIR/.travis/runAllUnitTests.scd"
