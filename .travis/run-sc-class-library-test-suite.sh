69dab1f42cd867753458fab3588dfe82c35ed22f
Brian Heim <brianlheim@gmail.com>
2017-09-03 15:22:36 -0400

    travis: try using start-stop-daemon instead

--- .travis/run-sc-class-library-test-suite.sh
+++ .travis/run-sc-class-library-test-suite.sh
@@ -4,16 +4,6 @@
 # Tests in testsuite/classlibrary/server are ignored,
 # as are tests for the UnitTesting Quark itself.
 
-# accommodate headless setup on Linux
-# copied from testsuite/sclang/launch_test.py
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]
-then
-	export QT_PLATFORM_PLUGIN=offscreen
-	export DISPLAY=:99:0
-	sh -e /etc/init.d/xvfb start
-	/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
-fi
-
 if "$SCLANG" "$TRAVIS_BUILD_DIR/.travis/configureTestEnvironment.scd"
 then
 	"$SCLANG" "$TRAVIS_BUILD_DIR/.travis/runAllUnitTests.scd"
