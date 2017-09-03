2905a11bd57724f64d17a434ddb0d9b96ea895b9
Brian Heim <brianlheim@gmail.com>
2017-09-03 14:39:42 -0400

    travis test script: attempt fix for starting supercollider on headless linux

--- .travis/run-sc-class-library-test-suite.sh
+++ .travis/run-sc-class-library-test-suite.sh
@@ -5,9 +5,13 @@
 # as are tests for the UnitTesting Quark itself.
 
 # accommodate headless setup on Linux
+# copied from testsuite/sclang/launch_test.py
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
 	export QT_PLATFORM_PLUGIN=offscreen
+	export DISPLAY=:99:0
+	sh -e /etc/init.d/xvfb start
+	/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 fi
 
 if "$SCLANG" "$TRAVIS_BUILD_DIR/.travis/configureTestEnvironment.scd"
