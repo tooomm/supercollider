accb9ebcd7b5542d50ea684dd828336e52a2cde3
Brian Heim <brianlheim@gmail.com>
2017-11-23 21:52:48 -0500

    travis: update qpm scripts
    Temporarily use brian heim's develop branch

--- .travis/qpm-prep.sh
+++ .travis/qpm-prep.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/BUILD
-sudo pip install git+https://github.com/scztt/qpm.git@qpm-unit
-qpm quark checkout CommonTests CommonTestsGUI API --location $HOME/Quarks
-cp ../travis_test_run_proto.json ./travis_test_run.json
+sudo pip install git+https://github.com/brianlheim/qpm.git@develop
+qpm quark checkout API --location $HOME/Quarks
--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks
+export QPM_SCLANG=$SCLANG
+TEST_SPEC=$TRAVIS_BUILD_DIR/travis_test_run_proto.json 
+TEST_INCLUDE="$HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary"
+qpm test.list --include $TEST_INCLUDE -o
+qpm test.run -l $TEST_SPEC -o --include $TEST_INCLUDE
