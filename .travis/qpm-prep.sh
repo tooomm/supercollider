b62f1b09ae91388174cd7efaf3fb2115b12a9580
Brian Heim <brianlheim@gmail.com>
2017-11-23 22:09:20 -0500

    switch to test-expander branch of brian's repo

--- .travis/qpm-prep.sh
+++ .travis/qpm-prep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $TRAVIS_BUILD_DIR/BUILD
-sudo pip install git+https://github.com/brianlheim/qpm.git@develop
+sudo pip install git+https://github.com/brianlheim/qpm.git@test-expander
 qpm quark checkout API --location $HOME/Quarks
