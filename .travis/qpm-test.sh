b8f9f845af3b46e9f55444427067a9af7e99a3c3
Brian Heim <brianlheim@gmail.com>
2017-11-24 15:02:40 -0500

    travis: reduce qpm output

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -3,5 +3,4 @@
 export QPM_SCLANG=$SCLANG
 TEST_SPEC=$TRAVIS_BUILD_DIR/travis_test_run_proto.json 
 TEST_INCLUDE="$HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary"
-qpm test.list -p $SCLANG --include $TEST_INCLUDE -o
-qpm test.run -l $TEST_SPEC -o -p $SCLANG --include $TEST_INCLUDE
+qpm test.run -l $TEST_SPEC -p $SCLANG --include $TEST_INCLUDE
