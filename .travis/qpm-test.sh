6d7e3355ae6bcbf7f198b61c94d777886312157e
Brian Heim <brianlheim@gmail.com>
2017-11-24 15:04:15 -0500

    travis: move json spec into .travis/

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export QPM_SCLANG=$SCLANG
-TEST_SPEC=$TRAVIS_BUILD_DIR/travis_test_run_proto.json 
+TEST_SPEC=$TRAVIS_BUILD_DIR/.travis/travis_test_run.json
 TEST_INCLUDE="$HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary"
 qpm test.run -l $TEST_SPEC -p $SCLANG --include $TEST_INCLUDE
